c9b1684ed082f030a890e313d980f60b87572678
Erkz <Erkiparna7@gmail.com>
2023-09-17 01:03:36 +0300

    Tarbitud elektrikulu arvutamine

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
 
+import customerController from "./controllers/Customer";
+import deviceController from "./controllers/Device";
+import usageController from "./controllers/Usage";
+
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+app.use('/', customerController);
+app.use('/', deviceController);
+app.use('/', usageController);
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
