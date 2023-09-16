fff2721fce99fa92edfb3f8623feebc06f315202
Erkz <Erkiparna7@gmail.com>
2023-09-17 01:05:25 +0300

    Elektrikulu küsimine

--- controllers/Usage.ts
+++ controllers/Usage.ts
@@ -113,4 +113,75 @@ router.put('/usage/:id', async (req: Request, res: Response) => {
     }
 })
 
+router.get('/usage-start-period', async (req: Request, res: Response) => {
+    try{
+        const data = await Usage.find();
+        const dataInPeriod = data.filter((usage: any) =>
+            new Date(usage.startDate).getTime() > new Date(req.body.from).getTime() &&
+            new Date(usage.startDate).getTime() < new Date(req.body.to).getTime())
+        res.json(dataInPeriod)
+    }
+    catch(error){
+        res.status(500).json({message: error})
+    }
+})
+
+router.get('/usage-end-period', async (req: Request, res: Response) => {
+    try{
+        const data = await Usage.find();
+        const dataInPeriod = data.filter((usage: any) =>
+            new Date(usage.endDate).getTime() > new Date(req.body.from).getTime() &&
+            new Date(usage.endDate).getTime() < new Date(req.body.to).getTime())
+        res.json(dataInPeriod)
+    }
+    catch(error){
+        res.status(500).json({message: error})
+    }
+})
+
+router.get('/usage-customer/:customer', async (req: Request, res: Response) => {
+    try{
+        const data = await Usage.find();
+        const dataInPeriod = data.filter((usage: any) =>
+            usage.customer.toString() === req.params.customer
+        )
+        res.json(dataInPeriod)
+    }
+    catch(error){
+        res.status(500).json({message: error})
+    }
+})
+
+router.get('/usage-customer-sum/:customer', async (req: Request, res: Response) => {
+    try{
+        const data = await Usage.find();
+        const dataInPeriod = data.filter((usage: any) =>
+            usage.customer.toString() === req.params.customer
+        )
+        let sum = 0;
+        dataInPeriod.forEach((usage: any) => sum += usage.totalUsageCost);
+        res.json(sum)
+    }
+    catch(error){
+        res.status(500).json({message: error})
+    }
+})
+
+router.get('/usage-customer-sum-period/:customer', async (req: Request, res: Response) => {
+    try{
+        const data = await Usage.find();
+        const dataInPeriod = data.filter((usage: any) =>
+            usage.customer.toString() === req.params.customer &&
+            new Date(usage.endDate).getTime() > new Date(req.body.from).getTime() &&
+            new Date(usage.endDate).getTime() < new Date(req.body.to).getTime()
+        )
+        let sum = 0;
+        dataInPeriod.forEach((usage: any) => sum += usage.totalUsageCost);
+        res.json(sum)
+    }
+    catch(error){
+        res.status(500).json({message: error})
+    }
+})
+
 export default router;
